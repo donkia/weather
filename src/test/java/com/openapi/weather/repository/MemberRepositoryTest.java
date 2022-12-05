fc4ffa402db00b998482be8ffc83342275bf2e53
kimbyeonghyeon <bhkim92@naver.com>
2022-12-05 21:49:47 +0900

    20221205 중복회원 프로세스 테스트

--- src/test/java/com/openapi/weather/repository/MemberRepositoryTest.java
+++ src/test/java/com/openapi/weather/repository/MemberRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.openapi.weather.repository;
 
 import com.openapi.weather.domain.Member;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -24,6 +25,23 @@ class MemberRepositoryTest {
 
         assertEquals(savedMember.getId(), member.getId());
         assertEquals(0, member.getImages().size());
+    }
+
+    @Test
+    void 중복멤버(){
+        Member member1 = Member.builder()
+                .email("11@naver.com")
+                .build();
+
+        memberRepository.save(member1);
+
+        Member member2 = Member.builder()
+                .email("11@naver.com")
+                .build();
+
+        memberRepository.findByEmail(member2.getEmail()).orElseThrow(() -> new IllegalArgumentException("중복된 이메일이 존재합니다"));
+
+
     }
 
 }
